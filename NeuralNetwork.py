Implemented a fix for backpropagation, able to solve xor.

NeuralNetwork.py
@@ -1,6 +1,8 @@
 import numpy as np
+from matplotlib import pyplot as plt
 
-from NN_Visualizations import graph_smoothed_values
+from NN_Visualizations import plot_smoothed
+
 
 class NeuralNetworkLayer:
 
@@ -17,12 +19,13 @@ class NeuralNetworkLayer:
         self.weights = 2 * np.random.random((input_size, output_size)) - 1
         self.bias = 2 * np.random.random(output_size) - 1
 
+        # Info about layer
         self.input_size = input_size
         self.output_size = output_size
 
         activation_functions_dictionary = {'sigmoid': self.sigmoid,
                                            'relu': self.relu}
-
+        self.activation_function_str = activation_function  # Name, or the string argument for the activation function.
         self.activation_function = activation_functions_dictionary[activation_function]
 
     # Activation functions
@@ -30,7 +33,7 @@ class NeuralNetworkLayer:
     def sigmoid(x, derivative=False):
         if derivative:
             return x * (1 - x)  # Note, the actual derivative is sig(x) * (1 - sig(x))
-        return 1 / (1 + np.exp(x))
+        return 1 / (1 + np.exp(-x))  # Missed the minus... spent 30-50 minutes debugging...
 
     @staticmethod
     def relu(x, derivative=False):
@@ -39,13 +42,18 @@ class NeuralNetworkLayer:
         return np.where(x > 0, x, 0)
 
     def feed_forward(self, input_values):
-        output_values = self.weights.dot(self.weights)
+        output_values = input_values.dot(self.weights)
         output_values += self.bias
         output_values = self.activation_function(output_values)
 
         return output_values
 
     def training_feed_forward(self, input_values):
+        """
+        Feedforward reserved for training purposes only. To get the output of the layer use self.feed_forward.
+        :param input_values:
+        :return:
+        """
         raw_output_values = input_values.dot(self.weights)
         raw_output_values += self.bias
 
@@ -71,7 +79,11 @@ class NeuralNetwork:
         # Diagnostic vars.
 
     def predict(self, input_values):
-
+        """
+        The feed-forward part of the neural network. It does not train the NN.
+        :param input_values:
+        :return:
+        """
         out = input_values
         for layer in self.layers:
             out = layer.feed_forward(out)
@@ -79,25 +91,47 @@ class NeuralNetwork:
         return out
 
     def epoch(self, input_values, desired_output_values, diagnostics=False):
+        """
+        Runs one training epoch on the given input and desired output values.
+        :param input_values: Input values can be a 1D array of the values OR an array containing multiple 1D arrays of
+        the inputs. You must provide as many desired output arrays as there are input arrays.
+        :param desired_output_values:
+        :param diagnostics: If True, it will return the error array.
+        :return: error array if diagnostics is True, else None.
+        """
         raw_outputs = []
+        act_outputs = []
         inputs = []
         # Feed forward part, to see how far off we are.
         out, raw_out = input_values, 0
         for layer in self.layers:
             inputs.append(out)
             out, raw_out = layer.training_feed_forward(out)
+            act_outputs.append(out)
             raw_outputs.append(raw_out)
 
         error = 0.5 * (desired_output_values - out) ** 2  # MSE
-        layer_error = out - desired_output_values
+        layer_error = desired_output_values - out
+        delta = layer_error
         # The backpropagation part
-        for layer, raw_out, inp in zip(reversed(self.layers), reversed(raw_outputs), reversed(inputs)):
-            delta = layer_error * layer.activation_function(raw_out, derivative=True)
+        for layer, raw_out, inp, act_out in zip(reversed(self.layers), reversed(raw_outputs),
+                                                reversed(inputs), reversed(act_outputs)):
+            # If the layer has a sigmoid activation function, use the activated outputs instead for the derivative.
+            if layer.activation_function_str == 'sigmoid':
+                delta *= layer.activation_function(act_out, derivative=True)
+            else:
+                delta *= layer.activation_function(raw_out, derivative=True)
+
             layer.bias += delta.sum(axis=0) * self.learning_rate
             layer.weights += inp.T.dot(delta) * self.learning_rate
 
+            delta = delta.dot(layer.weights.T)
+
         if diagnostics:
             return error
+
+
+# Test variables
 
 
 X = np.array([[0, 1],
@@ -110,14 +144,22 @@ Y = np.array([[1],
               [0],
               [0]])
 
-if __name__ == "__main__":
-    sample_network = NeuralNetwork([NeuralNetworkLayer(2, 3),
-                                    NeuralNetworkLayer(3, 1)])
+np.random.seed(0)
 
+if __name__ == "__main__":
+    sample_network = NeuralNetwork([NeuralNetworkLayer(2, 3, 'relu'),
+                                    NeuralNetworkLayer(3, 1, 'relu')],
+                                   learning_rate=0.1)
+
+    # A test which trains the NN on xor.
     errors = []
     for i in range(10000):
+        err = sample_network.epoch(X, Y, True)  # type: np.ndarray
+        errors.append(err.sum())
         if i % 1000 == 0:
-            errors.append(sample_network.epoch(X, Y, True))
-        sample_network.epoch(X, Y)
+            print(f"Epoch {i + 1} | Error: {err.sum()} | Progress: {i / 100}% Complete")
 
-    graph_smoothed_values(errors)
+    print(sample_network.predict(X))
+
+    plot_smoothed(errors, 1)
+    plt.show()